e44fc79f9ee76c3615255c7d7101bbe7b4f0b578
AngelCalderon105 <89942462+AngelCalderon105@users.noreply.github.com>
2024-09-08 00:21:30 -0700

    Tailwind Config Error Debugged

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+
 const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
@@ -7,17 +8,16 @@ const config: Config = {
   ],
   theme: {
     extend: {
-
       fontFamily: {
         inter: ['var(--font-inter)', 'sans-serif'],
         dmsans: ['var(--font-dmsans)', 'sans-serif'],
       },
-        colors: {
-          'light-green': '#74C69D',
-        },
-        animation: {
-          'float': 'float 2s infinite',
-        },
+      colors: {
+        'light-green': '#74C69D',
+      },
+      animation: {
+        'float': 'float 2.5s ease-in-out infinite',
+      },
       backgroundImage: {
         'custom-gradient-diagonal': 'linear-gradient(225deg, #ffffff 40%, #74C69D 80%)',
         'custom-gradient-diagonal-bottom': 'linear-gradient(195deg, #ffffff 40%, #74C69D 80%)',
@@ -29,13 +29,9 @@ const config: Config = {
           '50%': { transform: 'translateY(-30px)' },
         },
       },
-      colors: {
-        'light-green': '#74C69D',
-      },
-      animation: {
-        'float': 'float 2.5s ease-in-out infinite',
-      },
     },
-    plugins: [],
-  };
+  },
+  plugins: [],
+};
+
 export default config;
