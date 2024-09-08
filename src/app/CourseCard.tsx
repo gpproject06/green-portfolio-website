1c0704116b8bb60666a5812a370645007da8ccfa
AngelCalderon105 <89942462+AngelCalderon105@users.noreply.github.com>
2024-09-08 00:30:53 -0700

    Fixed Card Border Radius

--- src/app/CourseCard.tsx
+++ src/app/CourseCard.tsx
@@ -8,7 +8,7 @@ interface CourseCardProps {
 
 const CourseCard: React.FC<CourseCardProps> = ({ imageUrl, title, active }) => {
     return (
-        <div className={`font-bold text-center shadow-2xl w-60 mb-10 md:mx-6 text-sm lg:text-base xl:text-xl 
+        <div className={`font-bold text-center shadow-2xl w-60 mb-10 md:mx-6 text-sm lg:text-base xl:text-xl rounded-md 
             ${!active ? 'opacity-50' : ''} 
             transition-transform transform hover:scale-105`}>
             <Image 
@@ -17,7 +17,7 @@ const CourseCard: React.FC<CourseCardProps> = ({ imageUrl, title, active }) => {
                 width={400} 
                 height={400}
                 layout="responsive"
-                className='rounded-md'
+                className=''
             />
             <h1 className='p-3 text-lg'>{title}</h1>
         </div>
